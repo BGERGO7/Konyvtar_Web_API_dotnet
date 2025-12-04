5979c9c811bd7dd6d3ce78b825feb8da06270168
BGERGO7 <colt-reopen-tree@duck.com>
2025-12-04 22:26:12 +0100

    Updated GenreUpdateDto.cs

--- KonyvtarWebApi_BG/DTOs/Genre/GenreUpdateDto.cs
+++ KonyvtarWebApi_BG/DTOs/Genre/GenreUpdateDto.cs
@@ -3,7 +3,10 @@ using System.ComponentModel.DataAnnotations;
 namespace KonyvtarWebApi_BG.DTOs.Genre
 {
     public class GenreUpdateDto
-    {        
+    {
+        [Required]
+        public int GenreId { get; set; }
+
         [Required]
         [MaxLength(50)]
         public string GenreName { get; set; }
